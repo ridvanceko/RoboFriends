0b01f1214701fd445198469846aaa35bb36da18d
Ridvan Cekmez <ridvanceko@gmail.com>
2020-02-21 15:34:22 -0700

    components created

---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <div>
+      <img alt='robots' src=''></img>
+        <div>
+          <h2>Ceko</h2>
+          <p>ceko@email.com</p>
+        </div>
+
+    </div>
+  );
+}
+
+export default Card;
