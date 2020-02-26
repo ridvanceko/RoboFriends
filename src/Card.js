4835284596c236fe441d79a03bf1be147dd24d30
Ridvan Cekmez <ridvanceko@gmail.com>
2020-02-26 14:41:35 -0700

    robo api

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Card = () => {
   return (
-    <div>
-      <img alt='robots' src=''></img>
+    <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+      <img alt='robots' src='https://robohash.org/tester?200*200' />
         <div>
           <h2>Ceko</h2>
           <p>ceko@email.com</p>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import './index.css';
 import Card from './Card';
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<Card />, document.getElementById('root'));
+ReactDOM.render(
+<div>
+<Card />
+<Card />
+<Card />
+
+</div>
+
+, document.getElementById('root'));
 
 
