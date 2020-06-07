9862875ef85527f59d397e118d1ddc2a793feab4
Ridvan Cekmez <ridvanceko@gmail.com>
2020-06-07 16:53:08 -0700

    objects selected

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
       <img alt='robots' src='https://robohash.org/tester?200*200' />
         <div>
-          <h2>Ceko</h2>
-          <p>ceko@email.com</p>
+          <h2>{props.name}</h2>
+          <p>{props.email}</p>
         </div>
 
     </div>
---
+++ src/robots.js
@@ -0,0 +1,23 @@
+export const robots = [
+  {
+    id: 1,
+    name: 'Shiyu Cheng',
+    username: 'Cheng',
+    email: 'shiyulater@email.com'
+  },
+  {
+    id: 2,
+    name: 'Jesus Elquqer',
+    username: 'El Cucu',
+    email: 'jesus@email.com'
+  },
+  {
+    id: 3,
+    name: 'Hamrah',
+    username: 'Captan',
+    email: 'hamrah@email.com'
+  },
+  
+]
+
+export default robots;
